4982529243b37cae384f794ac1c1841676e4f3e3
Corey <coreyzhee@gmail.com>
2023-04-08 23:57:56 -0400

    not sure

--- RubyMonk/1.rb
+++ RubyMonk/1.rb
@@ -1,4 +1,10 @@
 # Everything is an object
 
+# Everything is an object in Ruby, but not everything is a method.
+# Examples of non-method: if,else, variables, class, modules
+
 puts self
-puts 2.even?
+puts 2.even?              # In this example, .even? is the method, and 2 is the object
+
+
+
