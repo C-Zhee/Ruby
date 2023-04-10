7c628a23e69750bc59392b9ba84d2d48871b3196
Corey <coreyzhee@gmail.com>
2023-04-09 23:57:37 -0400

    methods

--- RubyMonk/1.rb
+++ RubyMonk/1.rb
@@ -7,4 +7,7 @@ puts self
 puts 2.even?              # In this example, .even? is the method, and 2 is the object
 
 
-
+for i in 1..10            # For every number between 1-10 without 10.
+  next if i % 2 == 0      # skips the number if it has remainder of 0 (even)
+  puts i                  # prints every odd number
+end
