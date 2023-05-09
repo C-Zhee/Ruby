004e9adca1273395186bb7cadbf58f6a1284a163
Corey <coreyzhee@gmail.com>
2023-05-08 22:47:47 -0400

    Hello

--- RubyMonk/RubyPrimer/6.rb
+++ RubyMonk/RubyPrimer/6.rb
@@ -1,2 +1,25 @@
 # Arrays 
 
+
+
+# def please_play(games)
+#     puts "please play #{games} with me"
+# end
+
+# puts please_play(3)
+
+
+for i in 1..5
+  next if i % 2 == 0
+  print i
+end
+
+# print out Ruby! 30 times using loops
+
+# counter = 0
+
+# loop do
+# counter += 1
+# print "Ruby!"
+# break if counter == 30
+# end
